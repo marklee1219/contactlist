fd0fad1c3edfcd2af9fc5bec86baaf3c3a2970d6
Mark Lee <marklee1219@gmail.com>
2018-01-21 05:52:12 -0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{csrf_token()}}"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>ContactStore</title>
+</head>
+<body>
+    <div class="container">
+        <div id="app">
+            <contacts></contacts>
+        </div>
+    </div>
+
+    <script src="/js/app.js"></script>
+</body>
+</html>
